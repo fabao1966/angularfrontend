e1af43545174f0585ef67ecc2cf65802058f3f1a
fsilvaso <fsilvaso@indracompany.com>
2024-05-07 18:24:02 -0300

    listando musicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { inject } from '@angular/core';
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+import { MusicService } from './services/music.service';
+import { Music } from './models/music.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  musicas: Music[] = [];
+  private musicService = inject(MusicService);
+
+  constructor(){
+    this.obterMusicasCadastradas();
+  }
+
+  obterMusicasCadastradas(){
+    this.musicService.obterMusicas()
+    .subscribe(musicas => this.musicas = musicas)
+  }
 }
--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import {Injectable, inject} from "@angular/core";
+import { environment } from "../../environments/environment";
+import { Music } from "../models/music.model";
 @Injectable({
   providedIn: 'root'
 })
 
 export class MusicService{
   private httpCliente = inject(HttpClient);
+  private url = environment.api;
 
   obterMusicas(){
-
+    return this.httpCliente.get<Music[]>(this.url + 'musics');
   }
 }
